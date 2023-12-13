f6d6a61d20d4ade4088dc0d963a44038757b4e3a
James Rivera <112741353+JamesRiveran@users.noreply.github.com>
2023-12-13 15:27:16 -0600

    First commit

---
+++ src/main/java/com/mycompany/project1/Project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1;
+
+import Controller.ViewController;
+
+/**
+ *
+ * @author james
+ */
+public class Project1 {
+
+    public static void main(String[] args) {
+        Controller.ViewController view = new ViewController();
+        view.start();
+    }
+}
