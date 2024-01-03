6d233633a9ac0524cd13f14e2505a26b23dd9755
kevtico20 <jdhkevin@gmail.com>
2024-01-03 15:49:05 -0600

    Esto ya se arreglo lo del table en calibraciones

--- src/main/java/View/Modulo.java
+++ src/main/java/View/Modulo.java
@@ -26,7 +26,7 @@ public class Modulo extends javax.swing.JFrame {
      * Creates new form Modulo
      */
     public Modulo() {
-        initComponents();
+        initComponents  ();
         btnDeleteInstru.setEnabled(false);
         setIconImage(new javax.swing.ImageIcon("src/main/resources/images/Logo.png").getImage());
         setTitle("SILAB: Sistema de Laboratorio Industrial");
