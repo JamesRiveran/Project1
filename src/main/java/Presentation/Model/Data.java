b1f38c52010f6843d5f29fb7054243454eed30ab
James Rivera <112741353+JamesRiveran@users.noreply.github.com>
2024-01-30 02:41:28 -0600

    Ya funciona varios clientes

--- src/main/java/Presentation/Model/Data.java
+++ src/main/java/Presentation/Model/Data.java
@@ -4,22 +4,66 @@ package Presentation.Model;
 import Protocol.User;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class Data {
-    private List<User> users;
+    private List<String> nombresDisponibles;
+    private List<String> nombresEscogidos;
 
     public Data() {
-        users = new ArrayList<>();
-        users.add(new User("001","001","Juan"));
-        users.add(new User("002","002","Maria"));
-        users.add(new User("003","003","Pedro"));
+        this.nombresDisponibles = new ArrayList<>();
+        this.nombresEscogidos = new ArrayList<>();
+    }
+    
+    public String seleccionarUsuarioAlAzar() {
+        if (nombresDisponibles.isEmpty()) {
+            return "Todos los nombres han sido escogidos.";
+        }
+
+        Random random = new Random();
+        int indice = random.nextInt(nombresDisponibles.size());
+        String nombreElegido = nombresDisponibles.get(indice);
+        nombresDisponibles.remove(nombreElegido);
+        nombresEscogidos.add(nombreElegido);
+
+        return nombreElegido;
+    }//AÑADDIR VALIDACION EN CASO DE MAS CLIENTES DE LOS QUE SE PUEDE
+
+
+    public void cargarNombres() {
+        nombresDisponibles.add("James Rivera");
+        nombresDisponibles.add("Jose Vindas");
+        nombresDisponibles.add("Gabriel Madrigal");
+        nombresDisponibles.add("Kevin Fallas");
+        nombresDisponibles.add("Carlos Carranza");
+    }
+ 
+    public static String generarIdLocal() {
+        Random random = new Random();
+        StringBuilder consecutivo = new StringBuilder();
+
+        // Generar cadena de 6 números aleatorios
+        for (int i = 0; i < 6; i++) {
+            consecutivo.append(random.nextInt(10)); // Números del 0 al 9
+        }
+
+        return consecutivo.toString();
     }
 
-    public List<User> getUsers() {
-        return users;
+    public static String generarClaveSegura() {
+        // Generar una clave segura con BCrypt
+        String clavePlana = generarClaveAleatoria();
+        return BCrypt.hashpw(clavePlana, BCrypt.gensalt());
     }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
+    
+    private static String generarClaveAleatoria() {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder clave = new StringBuilder();
+        for (int i = 0; i < 9;i++) {
+            int indice = (int) (Math.random() * caracteres.length());
+            clave.append(caracteres.charAt(indice));
+        }
+        return clave.toString();
     }
 }
